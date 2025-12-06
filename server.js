2220d1c53b0d7caf5dc3f97879dfd1610c52791d
venuaa-cmd <venuaa@gmail.com>
2025-12-06 15:20:23 +0530

    Encode ticker in API request URL
    Use encodeURIComponent for the ticker in the API URL.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/api/stock', async (req, res) => {
 
     try {
         // IndianAPI Endpoint Structure
-        const url = `https://stock.indianapi.in/stock?name=${query}`;
+        const url = `https://stock.indianapi.in/stock?name=${encodeURIComponent(cleanTicker)}`;
         
         const response = await axios.get(url, {
             headers: {
